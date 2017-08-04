5b0b2d5c716ef0a81fcaf843cfcf2084f66a1576
amanfojnr <kenmarch@zoho.com>
2017-08-04 15:00:30 +0000

    completed viable functions

---
+++ quote_box/serializers.py
@@ -0,0 +1,8 @@
+from .models import Quote
+from rest_framework import serializers
+
+
+class QuoteSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Quote
+        fields = ('id', 'quote', 'source')
--- quote_box/views.py
+++ quote_box/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.renderers import JSONRenderer
+from .serializers import QuoteSerializer
+from .models import Quote
+from random import randrange
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+class QuoteListView(generics.ListAPIView):
+    """
+    API endpoint that generate quotes all quotes
+    """
+    queryset = Quote.objects.order_by('pk')
+    serializer_class = QuoteSerializer
+
+
+def random_quote_detail(request):
+    """
+    API endpoint that generates a random quote
+    """
+    quote = Quote.objects.get(id=randrange(1, 264))
+    serializer = QuoteSerializer(quote)
+    response = HttpResponse(JSONRenderer().render(serializer.data),
+                            content_type='application/json')
+    response["Access-Control-Allow-Origin"] = "*"
+    return response
+
+
+def index(request):
+    return render(request,
+                  "quote_box/index.html")
