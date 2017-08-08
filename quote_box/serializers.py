74600a7283c89a291bdb0f56c721f4f9e392ba8c
amanfojnr <kenmarch@zoho.com>
2017-08-08 20:29:51 +0000

    added more entry points and changed design'

--- quote_box/serializers.py
+++ quote_box/serializers.py
@@ -2,7 +2,7 @@ from .models import Quote
 from rest_framework import serializers
 
 
-class QuoteSerializer(serializers.HyperlinkedModelSerializer):
+class QuoteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Quote
         fields = ('id', 'quote', 'source')
